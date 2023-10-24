0a29a7026b2b1a32876fe6cda74c756cd2573c3f
Desi <141796712+DesiList753@users.noreply.github.com>
2023-10-23 23:39:33 -0300

    Add files via upload

--- certamen_2.1/usmweb/views.py
+++ certamen_2.1/usmweb/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Comunicado, Entidad
-# Create your views here.
+# Create your views here.   
 
 
 def index(request):
-    comunicados = Comunicado.objects.filter(visible=True).order_by('fecha_publicacion')
+    comunicados = Comunicado.objects.order_by('fecha_publicacion')
     entidades = Entidad.objects.order_by('nombre_entidad')
-    select_entidad = request.POST.get('s_entidad')
-    if request.method == 'POST':
+    select_entidad = request.GET.get('s_entidad')
+    if select_entidad:
         comunicados = comunicados.filter(entidad=select_entidad)
-        return render(request, 'usmweb/index.html',{'comunicados':comunicados,'entidades':entidades})
-    else:
-        return render(request, 'usmweb/index.html',{'comunicados':comunicados,'entidades':entidades})
-
-
-#haber analiza tienes 2 variables que entregas a el html, una que es la comunicados la cual en si transportaria los comunicados ordenados
+    return render(request, 'usmweb/index.html', {'comunicados': comunicados, 'entidades': entidades})
+#si uso data en el html voy a enredarme
